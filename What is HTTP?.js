e07f9a09135aa2a7872c8697035bc030a631879c
rossg2021 <77760650+rossg2021@users.noreply.github.com>
2021-06-15 21:04:50 -0400

    Add files via upload

---
+++ What is HTTP?.js
@@ -0,0 +1,28 @@
+// What is HTTP?
+// fetches resousrces other html pages, foundation of any data exchanges on the web, request are intiated by the receipent.
+
+// What is a URL?
+// mechanism used by browsers to retrieve published websiteaddress of a given unique site
+
+// What is DNS?
+// domain server name, translates domain names to ip addresses so browsers can load internet resources
+
+// What is a query string?
+// A query string is the portion of a URL where data is passed to a web application and/or back-end database
+
+// What are two HTTP verbs and how are they different?
+
+// What is an HTTP request?
+
+// a request that is made to a server which goes the http processes 
+
+// What is an HTTP response?
+// response from a server to a http request 
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// headers present information  about requests and response headers
+// examples: request headers: 
+
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+
+//   browser turns name into a ip address from the dns, browser request to ip address, server responds, then loads dom and uses js images css.
